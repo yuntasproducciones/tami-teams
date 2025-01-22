57c0ad9e49f3c7e1bb788c3c6b77a70031bb6c5e
Jonatan_Dev <105404210+jonatanLaureano@users.noreply.github.com>
2025-01-22 17:54:08 -0500

    update: se actualizo mejoras en el codigo

--- frontend/src/presentation/pages/AboutPage.tsx
+++ frontend/src/presentation/pages/AboutPage.tsx
@@ -3,6 +3,7 @@ import Footer from "../components/Footer";
 import ImageSection from "../components/nosotros/ImageSection";
 import PropositosSection from "../components/nosotros/PropositosSection";
 import Mision from "../components/nosotros/Mision";
+import Valores from "../components/nosotros/Valores";
 
 const AboutPage = () => {
   return (
@@ -11,6 +12,7 @@ const AboutPage = () => {
           <ImageSection />
           <PropositosSection />
           <Mision />
+          <Valores />
       <Footer />
     </div>
   );
