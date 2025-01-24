88720eaabb2b3c6fa0ac24f6c686be07d9e196d4
Jonatan_Dev <105404210+jonatanLaureano@users.noreply.github.com>
2025-01-24 18:18:17 -0500

    update se actualizo el codigo

--- frontend/src/presentation/pages/ProductsPage.tsx
+++ frontend/src/presentation/pages/ProductsPage.tsx
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ImgSec from "../components/productos/ImgSec";
+import Emprendimiento from "../components/productos/Emprendimiento";
 
 const ProductsPage = () => {
   return (
@@ -8,6 +9,9 @@ const ProductsPage = () => {
       <Navbar />
       <section>
         <ImgSec />
+      </section>
+      <section>
+        <Emprendimiento />
       </section>
       <Footer />
     </div>
