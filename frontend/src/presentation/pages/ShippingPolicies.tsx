ac24c691fb6b80aa6ddfade1cbbbe6f9ddb1eb2f
Diego Quipuzco <demential2003@gmail.com>
2025-01-24 20:51:29 -0500

    avance de politicas

--- frontend/src/presentation/pages/ShippingPolicies.tsx
+++ frontend/src/presentation/pages/ShippingPolicies.tsx
@@ -4,47 +4,46 @@ import enviosImage from "../../../public/assets/images/Envios.webp";
 
 const ShippingPolicies = () => {
   return (
-    <div>
+    <>
       <Navbar />
       <section
-        className="relative bg-cover bg-center h-[955px] pt-12 headerprueba"
+        className="relative bg-cover bg-center h-dvh pt-32 ps-20 flex flex-col justify-center text-white"
         style={{
-          backgroundImage: `linear-gradient(to bottom, rgb(16, 235, 231) 2%, rgba(0, 158, 155, 0.5) 30%, rgba(0, 158, 155, 0) 100%), url(${enviosImage})`,
+          backgroundImage: `linear-gradient(to bottom, rgba(15, 118, 110), rgba(3, 112, 112, .6) 75%, rgba(0, 0, 0, 0.75) 100%), url(${enviosImage})`,
         }}
       >
-        <div className="relative z-10 flex flex-col items-start justify-center h-full text-white p-12">
-          <div className="mb-4">
-            <svg
-              className="w-16 h-16"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M8 19H16M10 15L12 17L14 15M12 17V11M20 9H4M4 9L6 17H18L20 9M4 9L6 5H18L20 9"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-          </div>
-          <h1 className="text-6xl font-bold mb-4">
-            POLÍTICA
-            <br />
-            DE ENVÍOS
-          </h1>
-          <p className="text-xl mb-8">
-            Descubre nuestras políticas de envío,
-            <br />
-            pensadas para garantizar una experiencia
-            <br />
-            confiable y segura en cada pedido.
-          </p>
-        </div>
+        <svg
+          className="w-16 h-16"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M8 19H16M10 15L12 17L14 15M12 17V11M20 9H4M4 9L6 17H18L20 9M4 9L6 5H18L20 9"
+            stroke="currentColor"
+            strokeWidth="1"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+        <h1
+          style={{ textShadow: "rgba(0,0,0,.2) 1px 1px 20px" }}
+          className="text-6xl font-extrabold mb-10"
+        >
+          POLÍTICA
+          <br />
+          DE ENVÍOS
+        </h1>
+        <p className="text-2xl font-bold">
+          Descubre nuestras políticas de envío,
+          <br />
+          pensadas para garantizar una experiencia
+          <br />
+          confiable y segura en cada pedido.
+        </p>
       </section>
       <Footer />
-    </div>
+    </>
   );
 };
 
