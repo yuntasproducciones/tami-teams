8ac5a69c534b33bed38d16c24a276df37f5d9d23
Diego Quipuzco <demential2003@gmail.com>
2025-02-04 13:21:31 -0500

    HomePage no se usa y Home fue formateado

--- frontend/src/presentation/pages/Home.tsx
+++ frontend/src/presentation/pages/Home.tsx
@@ -1,24 +1,23 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Inicio/Hero.tsx";
 import MainSeccion from "../components/MainSection";
-import  Bienvenida  from "../components/Bienvenida";
+import Bienvenida from "../components/Bienvenida";
 import Footer from "../components/Footer";
 import PorQueNosotros from "../components/PorQueNosotros";
 import { Comentarios } from "../components/Comentarios.tsx";
 
-
 const Home = () => {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-            <MainSeccion />
-            <Bienvenida />
-          <PorQueNosotros />
-          <Comentarios />
-          <Footer />
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <MainSeccion />
+      <Bienvenida />
+      <PorQueNosotros />
+      <Comentarios />
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
