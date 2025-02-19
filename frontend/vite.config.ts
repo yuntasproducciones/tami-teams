5a27491d37824aec1e10bcffdfc74685a36c8b7a
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2025-02-19 10:41:17 -0500

    Eliminar el parámetro 'command' de la configuración de Vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
-export default defineConfig(({ command, mode }) => {
+export default defineConfig(({ mode }) => {
   // Cargar variables de entorno desde el directorio raíz del proyecto
   const env = loadEnv(mode, process.cwd(), '');
 
