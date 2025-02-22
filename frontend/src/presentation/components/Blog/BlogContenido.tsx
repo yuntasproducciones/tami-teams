c408daeabd6fab422ad3a53805ec2d65207748d7
Anstro <anstrosolano@gmail.com>
2025-02-21 19:12:40 -0500

    Creando componente articulos blog
    Tambien se implemento logica de navegacion, por lo que cada pestaña implementara una navegacion en caso se implemente backend

--- frontend/src/presentation/components/Blog/BlogContenido.tsx
+++ frontend/src/presentation/components/Blog/BlogContenido.tsx
@@ -1,195 +1,86 @@
-/** 
- * Componente que muestra el contenido de la sección de blog.
- * Muestra una imagen superior, una lista de artículos y una imagen inferior.
- * Cada artículo tiene una imagen, un título, una descripción y un contador de likes
- *  comentarios y compartidos.
- */
-
 import { Link } from "react-router-dom";
-import { FaHeart, FaComment, FaShare, FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { FaSearch } from "react-icons/fa";
 import bambu from "../../../../public/assets/images/blog/Bambu.jpg";
 import maquina from "../../../../public/assets/images/blog/MaquinaEmbalaje.png";
 import selladora from "../../../../public/assets/images/blog/Selladora.jpg";
-import imagen_superior from "../../../../public/assets/images/blog/entrada-blog/IMAGEN_SUPERIOR_TAMI.webp";
-import imagen_inferior from "../../../../public/assets/images/blog/entrada-blog/IMAGEN_INFERIOR_TAMI.webp";
+
+const articles = [
+  {
+    title: "Panel de Fibra de Bambú: Sostenibilidad y Estética para la construcción moderna",
+    description: "Futuro verde en la construcción. Beneficios del bambú.",
+    image: bambu,
+    category: "CATEGORÍA",
+    link: "/blog/detalle/bambu",
+  },
+  {
+    title: "Diseño Sostenible: Interiores Eco-Friendly para Oficinas Modernas",
+    description: "Descubre cómo incorporar materiales reciclados y energías renovables.",
+    image: maquina,
+    category: "CATEGORÍA",
+    link: "/blog/detalle/maquina",
+  },
+  {
+    title: "Iluminación Inteligente: Optimizando Espacios de Trabajo",
+    description: "Explora las últimas tendencias en iluminación LED y sistemas de control.",
+    image: selladora,
+    category: "CATEGORÍA",
+    link: "/blog/detalle/selladora",
+  },
+];
 
 const BlogContenido = () => {
   return (
-    <div className="w-full">
-      {/* Contenedor 1: Imagen superior con barra lateral y texto en color verde turquesa */}
-      <div className="relative w-full">
-        <img
-          src={imagen_superior}
-          alt="Imagen Superior"
-          className="w-full h-full object-cover"
-        />
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="flex items-center">
-            {/* Barra lateral */}
-            <span className="block w-1 h-12 bg-verde_turquesa mr-4"></span>
-            {/* Texto */}
-            <h2 className="text-3xl md:text-5xl font-bold text-verde_turquesa text-center">
-              NOTICIAS Y ARTICULOS RECIENTES
-            </h2>
-          </div>
+    <div className="p-6 max-w-6xl mx-auto flex gap-8 my-10">
+      <div className="w-3/4">
+        <h2 className="text-2xl font-bold mb-4">Todos los artículos</h2>
+        <div className="space-y-6">
+          {articles.map((article, index) => (
+            <Link to={article.link} key={index} className="block">
+              <div className="flex bg-teal-700 text-white rounded-lg overflow-hidden shadow-lg h-40">
+                <div className="w-1/3 h-full flex-shrink-0">
+                  <img src={article.image} alt={article.title} className="w-full h-full object-cover" />
+                </div>
+                <div className="p-4 w-2/3 flex flex-col justify-center">
+                  <h3 className="text-lg font-bold">{article.title}</h3>
+                  <p className="text-sm mt-2">{article.description}</p>
+                  <span className="text-xs bg-white text-teal-700 px-2 py-1 rounded mt-3 self-start">
+                    {article.category}
+                  </span>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
       </div>
-
-      {/* Contenedor 2: Artículos en bloque horizontal, apilados verticalmente */}
-      <div className="container mx-auto px-4 py-8">
-        <div className="flex flex-col items-center justify-center gap-8">
-          {/* Artículo 1 */}
-          <Link
-            to="/blog/detalle/bambu"
-            className="w-full max-w-5xl border-4 border-verde_turquesa rounded-3xl overflow-hidden transform transition-transform duration-300 hover:scale-105"
-          >
-            {/* Bloque horizontal */}
-            <div className="flex h-[300px]">
-              {/* Imagen a la izquierda */}
-              <div className="w-1/2 h-full">
-                <img
-                  className="w-full h-full object-cover"
-                  src={bambu}
-                  alt="Bambu"
-                />
-              </div>
-              {/* Texto a la derecha */}
-              <div className="w-1/2 p-6 flex flex-col justify-center">
-                <div className="font-bold text-2xl mb-2 text-verde_turquesa">
-                  Panel de fibra de bambú
-                </div>
-                <p className="text-gray-700 text-lg">
-                  Sostenibilidad y estética para la construcción moderna.
-                </p>
-                <div className="flex items-center space-x-6 mt-4 text-gray-600">
-                  <div className="flex items-center space-x-1">
-                    <FaHeart size={20} />
-                    <span>20</span>
-                  </div>
-                  <div className="flex items-center space-x-1">
-                    <FaComment size={20} />
-                    <span>5</span>
-                  </div>
-                  <div className="flex items-center space-x-1">
-                    <FaShare size={20} />
-                    <span>3</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
-
-          {/* Artículo 2 */}
-          <Link
-            to="/blog/detalle/maquina"
-            className="w-full max-w-5xl border-4 border-verde_turquesa rounded-3xl overflow-hidden transform transition-transform duration-300 hover:scale-105"
-          >
-            <div className="flex h-[300px]">
-              <div className="w-1/2 h-full">
-                <img
-                  className="w-full h-full object-cover"
-                  src={maquina}
-                  alt="Maquina"
-                />
-              </div>
-              <div className="w-1/2 p-6 flex flex-col justify-center">
-                <div className="font-bold text-2xl mb-2 text-verde_turquesa">
-                  Máquina de embalaje
-                </div>
-                <p className="text-gray-700 text-lg">
-                  Eficiencia y protección en cada empaque.
-                </p>
-                <div className="flex items-center space-x-6 mt-4 text-gray-600">
-                  <div className="flex items-center space-x-1">
-                    <FaHeart size={20} />
-                    <span>15</span>
-                  </div>
-                  <div className="flex items-center space-x-1">
-                    <FaComment size={20} />
-                    <span>2</span>
-                  </div>
-                  <div className="flex items-center space-x-1">
-                    <FaShare size={20} />
-                    <span>1</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
-
-          {/* Artículo 3 */}
-          <Link
-            to="/blog/detalle/selladora"
-            className="w-full max-w-5xl border-4 border-verde_turquesa rounded-3xl overflow-hidden transform transition-transform duration-300 hover:scale-105"
-          >
-            <div className="flex h-[300px]">
-              <div className="w-1/2 h-full">
-                <img
-                  className="w-full h-full object-cover"
-                  src={selladora}
-                  alt="Selladora"
-                />
-              </div>
-              <div className="w-1/2 p-6 flex flex-col justify-center">
-                <div className="font-bold text-2xl mb-2 text-verde_turquesa">
-                  Sellador de botellas
-                </div>
-                <p className="text-gray-700 text-lg">
-                  La solución ideal para garantizar la calidad de tus productos.
-                </p>
-                <div className="flex items-center space-x-6 mt-4 text-gray-600">
-                  <div className="flex items-center space-x-1">
-                    <FaHeart size={20} />
-                    <span>30</span>
-                  </div>
-                  <div className="flex items-center space-x-1">
-                    <FaComment size={20} />
-                    <span>8</span>
-                  </div>
-                  <div className="flex items-center space-x-1">
-                    <FaShare size={20} />
-                    <span>4</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </Link>
+      <div className="w-1/4">
+        <div className="relative mb-6">
+          <input 
+            type="text" 
+            placeholder="Buscar..." 
+            className="w-full p-2 border border-gray-300 rounded-lg pl-10"
+          />
+          <FaSearch className="absolute left-3 top-3 text-gray-500" />
         </div>
-
-        {/* Paginador: "Página X" con dos flechas */}
-        <div className="flex justify-center items-center space-x-6 mt-8 text-black">
-          {/* Flecha Izquierda */}
-          <button className="text-black">
-            <FaArrowLeft size={24} />
-          </button>
-
-          {/* Texto de la página */}
-          <span className="text-lg font-semibold">
-            Página 1
-          </span>
-
-          {/* Flecha Derecha */}
-          <button className="text-black">
-            <FaArrowRight size={24} />
-          </button>
-        </div>
-      </div>
-
-      {/* Contenedor 3: Imagen inferior */}
-      <div className="w-full">
-        <img
-          src={imagen_inferior}
-          alt="Imagen Inferior"
-          className="w-full h-full object-cover"
-        />
+        <h3 className="text-lg font-bold mb-3">CATEGORÍAS</h3>
+        <ul className="text-sm space-y-2 text-teal-700">
+          <li><Link to="#">SELLADOR DE BOTELLAS</Link></li>
+          <li><Link to="#">MÁQUINA DE EMBALAJE</Link></li>
+          <li><Link to="#">SELLADORA DE VASOS</Link></li>
+          <li><Link to="#">SELLADORA DE BOLSAS</Link></li>
+          <li><Link to="#">SOLDADORA LINGBA</Link></li>
+          <li><Link to="#">SOLDADORA SPARK</Link></li>
+          <li><Link to="#">PANEL DE FIBRA DE BAMBO</Link></li>
+          <li><Link to="#">VENTILADOR HOLOGRÁFICO</Link></li>
+          <li><Link to="#">WPC FENCE PANEL</Link></li>
+          <li><Link to="#">WPC WALL PANEL</Link></li>
+          <li><Link to="#">PURIFICADOR DE AGUA</Link></li>
+        </ul>
       </div>
     </div>
   );
 };
 
 export default BlogContenido;
-
-
 
 
 
